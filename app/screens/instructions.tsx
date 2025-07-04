d5d320d4f005d6b2be2ac696b211136120e93053
Yusuf Shihata <yusufshihata2006@gmail.com>
2025-07-04 15:36:01 +0300

    adding a basic card

--- app/screens/instructions.tsx
+++ app/screens/instructions.tsx
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import Bar from '../components/bar'
+import Card from '../components/card'
 
 const Instructions = () => {
     return(
         <View>
             <Bar />
-            <Text>This is Instructions.</Text>
+            <View style={styles.main}>
+                <Card title="Some Instruction" describtion="Its Describtion" />
+            </View>
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    screen: {
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    main: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+        paddingVertical: "25%",
+    }
+});
 
 export default Instructions;
