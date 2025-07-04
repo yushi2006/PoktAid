17f3a17bc788801afe04578ae19e868c12d7dd2b
Yusuf Shihata <yusufshihata2006@gmail.com>
2025-07-04 15:15:30 +0300

    finishing the simple app bar

--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Text, View, Button } from 'react-native'
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native'
+import Bar from '../components/bar'
 
 const Home = () => {
     const navigator = useNavigation();
     return (
         <View>
+            <Bar />
             <Text>This is the Home page.</Text>
             <Button title='Instructions' onPress={() => navigator.navigate('Instructions')} />
         </View>
