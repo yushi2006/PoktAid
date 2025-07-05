976a92be396f979dd6e362fc3bf56c7f34a71d87
Yusuf Shihata <yusufshihata2006@gmail.com>
2025-07-05 10:14:12 +0300

    adding the camera and audio button and finishing basic home page

--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -1,17 +1,51 @@
 import React, { Component } from 'react'
-import { Text, View, Button } from 'react-native'
+import { Text, View, Button, StyleSheet } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import Bar from '../components/bar'
+import InputButton from '../components/inputButton'
 
 const Home = () => {
     const navigator = useNavigation();
     return (
         <View>
             <Bar />
-            <Text>This is the Home page.</Text>
-            <Button title='Instructions' onPress={() => navigator.navigate('Instructions')} />
+            <View style={styles.textContainer}>
+                <Text style={styles.header}>PoktAid</Text>
+                <Text style={styles.noworry}>No need to panic, you won't die.</Text>
+            </View>
+            <View style={styles.container}>
+                <InputButton isCamera={true} />
+                <InputButton isCamera={false} />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: '10%',
+        paddingBottom: '10%',
+        width: '100%',
+        height: '80%',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#2596be',
+    },
+    textContainer: {
+        width: '100%',
+        padding: 10,
+        alignItems: 'center',
+    },
+    header: {
+        color: '#2596be',
+        fontWeight: 'bold',
+        fontSize: 32,
+    },
+    noworry: {
+        color: 'gray',
+    },
+});
 
 export default Home
--- app/screens/instructions.tsx
+++ app/screens/instructions.tsx
@@ -1,20 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import Bar from '../components/bar'
 import Card from '../components/card'
 import NavigationButton from '../components/navigationButton'
 
 const Instructions = () => {
+    const instructions = [
+        { id: 1, title: "Step 1", text: "Do the first thing." },
+        { id: 2, title: "Step 2", text: "Now do this." },
+        { id: 3, title: "Step 3", text: "Finally, complete this step." }
+    ];
+
+    const [idx, setIdx] = useState(0);
+
     return(
         <View style={styles.screen}>
             <Bar />
             <View style={styles.main}>
                 <View style={styles.container}>
-                    <Card title="Some Instruction" describtion="Its Describtion" />
+                    <Card title={instructions[idx]['title']} describtion={instructions[idx]['text']} />
                 </View>
                 <View style={styles.navigations}>
-                    <NavigationButton isBackButton={true} />
-                    <NavigationButton isBackButton={false} />
+                    {idx > 0 ?(
+                        <NavigationButton isBackButton={true} _press_fn={() => setIdx(idx => idx - 1)} />
+                    ): (<View style={styles.space} />)
+                    }
+                    {idx < instructions.length - 1 ?
+                        (<NavigationButton isBackButton={false} _press_fn={() => setIdx(idx => idx + 1)} />): (<View style={styles.space} />)
+                    }
                 </View>
             </View>
         </View>
@@ -41,7 +54,13 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         width: '100%',
         justifyContent: 'space-evenly',
-    }
+    },
+    space: {
+        paddingHorizontal: '15%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
 });
 
 export default Instructions;
